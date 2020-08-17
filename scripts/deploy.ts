13f4aaf489343ceae815fdcd68dfa9e491304fe6
Tarun Bansal <tarunbhm@gmail.com>
2020-08-17 22:49:04 +0530

    Add deploy script for kovan deployment

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,16 +1,19 @@
 import { ethers } from "@nomiclabs/buidler";
 
+// Kovan minion address
+const minionAddress = "0x98B550E95E90ADFA6D9841fAB937D81FcFEab6D2";
+
 async function main() {
-  const factory = await ethers.getContract("MinionTokenMinter");
+  const factory = await ethers.getContractFactory("MinionNFT");
 
   // If we had constructor arguments, they would be passed into deploy()
-  const contract = await factory.deploy();
+  const contract = await factory.deploy("Welcome NFT", "DWT", minionAddress);
 
   // The address that the Contract WILL have once mined
-  console.log(contract.address);
+  console.log("Deployed contract at:", contract.address);
 
   // The transaction that was sent to the network to deploy the Contract
-  console.log(contract.deployTransaction.hash);
+  console.log("Deployment transaction hash is:", contract.deployTransaction.hash);
 
   // The contract is NOT deployed yet; we must wait until it is mined
   await contract.deployed();
@@ -22,3 +25,4 @@ main()
     console.error(error);
     process.exit(1);
   });
+
